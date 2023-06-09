8e0e4a4486fd60a0e5b2bae80f78b66b9ce91e16
roope <kantolar@uef.fi>
2023-06-09 12:42:10 +0300

    lisätty osion 1 tehtävät 1-1 ja 1-2

--- Osio_01/app1/tehtava-1-2/App.js
+++ Osio_01/app1/tehtava-1-2/App.js
@@ -1,18 +1,13 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
         <Header heading={course} />
-        <Content part={part1} exercises={exercises1}/>
-        <Content part={part2} exercises={exercises2}/>
-        <Content part={part3} exercises={exercises3}/>
+        <Content/>
         <Total sum={exercises1+exercises2+exercises3}/>
     </div>
   )
@@ -36,10 +31,27 @@ const Total = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
+  console.log("Taalla ollaan ")
   return (
     <p>
-        {props.part} {props.exercises}
+      {props.part} {props.exercises}    
+    </p>
+  )
+}
+
+const Content = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <p>
+        <Part part={part1} exercises={exercises1}/>
+        <Part part={part2} exercises={exercises2}/>
+        <Part part={part3} exercises={exercises3}/>
     </p>
   )
 }
